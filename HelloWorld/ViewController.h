6f2c20ddd82dafcb51a3b8b63e4ab893f33357f8
Varun Murali <originate@Originates-MacBook-Pro.local>
2013-05-23 18:20:08 -0700

    Initial Commit

---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Varun Murali on 5/23/13.
+//  Copyright (c) 2013 Originate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HelloWorldTests/HelloWorldTests.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldTests.h
+//  HelloWorldTests
+//
+//  Created by Varun Murali on 5/23/13.
+//  Copyright (c) 2013 Originate. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HelloWorldTests : SenTestCase
+
+@end
