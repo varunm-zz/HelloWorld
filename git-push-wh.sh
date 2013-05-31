f759a564edd348c8622050fc93e647482000f35e
varunm <varun.murali@gmail.com>
2013-05-31 13:48:32 -0700

    edited

--- git-push-wh.sh
+++ git-push-wh.sh
@@ -9,7 +9,7 @@ POST_PUSH="$GIT_DIR_/hooks/post-push"
 test -x "$PRE_PUSH" &&
     exec "$PRE_PUSH" "$BRANCH" "$@"
 
-git push "$@"
+git push origin dev"$@"
 
 test $? -eq 0 && test -x "$POST_PUSH" &&
     exec "$POST_PUSH" "$BRANCH" "$@"
