f1c02c036d3d90cb591d5eb6be43ce86427bba42
varunm <varun.murali@gmail.com>
2013-06-06 11:07:56 -0700

    permissions

--- build/HelloWorld.build/Debug-iphonesimulator/HelloWorldTests.build/Script-09E70126174EF7C800DFCE82.sh
+++ build/HelloWorld.build/Debug-iphonesimulator/HelloWorldTests.build/Script-09E70126174EF7C800DFCE82.sh
@@ -10,11 +10,10 @@ our_env=("--setenv" "DYLD_INSERT_LIBRARIES=/../../Library/PrivateFrameworks/IDEB
 our_env=("${our_env[@]}" "--setenv" "XCInjectBundle=${our_test_bundle_path}")
 our_env=("${our_env[@]}" "--setenv" "XCInjectBundleInto=${TEST_HOST}")
 our_app_location="$(dirname "${TEST_HOST}")"
-/usr/local/bin/ios-sim launch "${our_app_location}" "${our_env[@]}" --args -SenTest All "${our_test_bundle_path}" --exit
+"${SRCROOT}/ios-sim" launch "${our_app_location}" "${our_env[@]}" --args -SenTest All "${our_test_bundle_path}" --exit
 killall -m -KILL "iPhone Simulator" || true
 exit 0
 else
 #   Running under Xcode
 "${DEVELOPER_TOOLS_DIR}/RunUnitTests"
 fi
-
